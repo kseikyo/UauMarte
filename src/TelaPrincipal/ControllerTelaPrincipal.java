8701bf6c014c4ea16510d8b8ad11c0a817081648
Lucas Emanuel <29212286+kseikyo@users.noreply.github.com>
2018-09-21 15:05:36 -0300

    Updating this branch

--- src/TelaPrincipal/ControllerTelaPrincipal.java
+++ src/TelaPrincipal/ControllerTelaPrincipal.java
@@ -1,10 +1,10 @@
 package TelaPrincipal;
 
+import CadastroProdutos.TelaCadastro;
 import ControllerClass.ControllerStart;
 import TelaLogin.ControllerTelaLogin;
 import javafx.application.Application;
 import javafx.fxml.FXML;
-import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,6 +21,12 @@ public class ControllerTelaPrincipal extends Application {
     public void f_GoToLogin() {
         ControllerTelaLogin controllerTelaLogin = new ControllerTelaLogin();
         controllerTelaLogin.start(controllerStart.getStage());
+    }
+
+    @FXML
+    public void f_GoToCadastroP() throws Exception{
+        TelaCadastro telaCadastro = new TelaCadastro();
+        telaCadastro.start(controllerStart.getStage());
     }
 
 }
