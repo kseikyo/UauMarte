2d702a6faa9d85b5c6bb7417dcad7a3172394430
Lucas Emanuel <29212286+kseikyo@users.noreply.github.com>
2018-09-21 14:17:42 -0300

    Tela inicial para tela login

--- src/TelaPrincipal/ControllerTelaPrincipal.java
+++ src/TelaPrincipal/ControllerTelaPrincipal.java
@@ -1,46 +1,26 @@
 package TelaPrincipal;
 
+import ControllerClass.ControllerStart;
 import TelaLogin.ControllerTelaLogin;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Tab;
-import ControllerClass.ControllerStart;
+import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class ControllerTelaPrincipal extends Application{
+
+public class ControllerTelaPrincipal extends Application {
     private ControllerStart controllerStart = new ControllerStart();
 
-    @FXML
-    private Tab tab_CadastroProduto;
-
-    @FXML
-    private Tab tab_Sair;
-
-    public void start(Stage stage) {
-        try {
-            controllerStart.initScreen("/FXMLFILES/TelaInicial.fxml","UAUMARTE", controllerStart.getStage());
-        } catch(IOException e) { }
+    public void start(Stage stage)throws IOException {
+        controllerStart.initScreen("/FXMLFILES/TelaInicial.fxml", "UAUMARTE", controllerStart.getStage());
     }
-
+    
     @FXML
-    void f_AbrirCadastroProduto(ActionEvent event) {
-
-    }
-
-    @FXML
-    void f_AbrirTelaLogin(ActionEvent event)throws IOException {
-
+    public void f_GoToLogin() {
         ControllerTelaLogin controllerTelaLogin = new ControllerTelaLogin();
         controllerTelaLogin.start(controllerStart.getStage());
     }
-
-
 
 }
